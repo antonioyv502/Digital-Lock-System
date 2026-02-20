3989a88b966738941075613da639ccad6d72eedd
antoniov$502 <bryanvasquez797@gmail.com>
2026-02-19 20:25:28 -0800

    Fix formatting and remove unnecessary whitespace

--- Top_Level_Module/digital_lock_top.v
+++ Top_Level_Module/digital_lock_top.v
@@ -1,13 +1,8 @@
 module digital_lock_top(SW, LEDR);
 
-
 input  [9:0] SW;
 output [9:0] LEDR; 
 
-
-
-
 digital_lock U1 (.clk(SW[6]), .reset(SW[0]), .x(SW[4:2]), .y(LEDR[0]),  .state(LEDR[3:2]));
-
 
 endmodule 
