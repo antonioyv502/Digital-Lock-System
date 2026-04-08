9ea958107901a6a336a9adb8724533718e063514
antoniov$502 <bryanvasquez797@gmail.com>
2026-04-08 15:17:17 -0700

    Update digital_lock_top.v

--- Top_Level_Module/digital_lock_top.v
+++ Top_Level_Module/digital_lock_top.v
@@ -1,13 +1,11 @@
-module digital_lock_top(KEY, SW, LEDR);
+module digital_lock_top(MAX10_CLK1_50, SW, LEDR);
 
-input  [1:0] KEY; //pushbuttons 
+input        MAX10_CLK1_50; //50MHz clk
 input  [9:0] SW;
-output [9:0] LEDR; 
+output [9:0] LEDR;
 
-wire clk;
 
-assign clk = KEY[0]; 
+digital_lock U1 (.clk(MAX10_CLK1_50), .reset(SW[0]), .x(SW[3:1]), .y(LEDR[0]),  .state(LEDR[3:2]), .pulse_led(LEDR[7]));
 
-digital_lock U1 (.clk(clk), .reset(SW[0]), .x(SW[3:1]), .y(LEDR[0]),  .state(LEDR[3:2]));
 
 endmodule 
