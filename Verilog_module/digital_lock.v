52a8ca84bbf118f5cb3759c3fa640b9fe17874a6
antoniov$502 <bryanvasquez797@gmail.com>
2026-04-08 15:19:43 -0700

    Refactor digital_lock module with pulse_led output

--- Verilog_module/digital_lock.v
+++ Verilog_module/digital_lock.v
@@ -1,76 +1,103 @@
-module digital_lock (clk, reset, x, y, state);
+module digital_lock(clk, reset, x, y, state, pulse_led);
   
-  input        clk;
-  input        reset; // active high reset
-  input [2:0]  x;
+  input        clk;   //clock signal
+  input        reset; //Aysnchronous active high reset
+  input [2:0]  x;     //3 bit input 
   output reg   y;
   output [1:0] state;
-
-     //SystemVerilog 
-    //Defining the states
+  output       pulse_led;
+  
+    // Defining the states
+  	//System Verilog 
     /*typedef enum logic [1:0] {
         S0 = 2'b00,
         S1 = 2'b01,
         S2 = 2'b10,
         S3 = 2'b11
     } state;
-    state current_state, next_state;*/
 
+    state current_state, next_state;*/
+    
+    parameter MAX_COUNT = 100_000_000; // 2 second 
+    reg [26:0] counter;
+    reg pulse = 0;
+
+    assign pulse_led = pulse;
+
+    
+    //Counter and pulse generation
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            counter <= 0;
+            pulse <= 0;
+        end else if (counter == MAX_COUNT) begin
+            counter <= 0; 
+            pulse <= 1;       
+        end else begin
+            counter <= counter + 1;
+            pulse <= 0;      
+        end
+    end
+
+    
+    
+   
     //Defining States
-    parameter S0 = 2'b00;
-    parameter S1 = 2'b01;
-    parameter S2 = 2'b10;
-    parameter S3 = 2'b11;
+    parameter S_0 = 2'b00;  
+    parameter S_1 = 2'b01;
+    parameter S_2 = 2'b10;
+    parameter S_3 = 2'b11;
 
     // State registers
     reg [1:0] current_state;
     reg [1:0] next_state;
-
-    assign state = current_state; //allows us to be able to see the current state of flip flops
+    
+    //allows us to be able to see the current state of flip flops and helpful for debugging
+    assign state = current_state; 
+    
     
   
-  // State transition logic (clocked)
-  always @(posedge clk or posedge reset) begin // asynchronous reset 
-        if (reset) //active high reset
-            current_state <= S0; // allows us to set to a known state
-        else
+    // State transition logic (clocked)
+    always @(posedge clk or posedge reset) begin  //active high reset
+        if (reset)
+            current_state <= S_0;   
+        else if (pulse)
             current_state <= next_state;
-  end
+    end
   
   
   // Combinational logic for next state
-  always @(*) begin
-    case (current_state)
-        S0: 
-            if (x == 3'b011)
-                next_state = S1;
-            else
-                next_state = S0;
-
-        S1: 
-            if (x == 3'b111)
-                next_state = S2;
-            else
-                next_state = S0;
-
-       
-        S2: 
-            if (x == 3'b101)
-                next_state = S3;
-            else
-                next_state = S0;
-
-        S3: 
-            next_state = S0; //
-
-        default: 
-            next_state = S0; // 
-    endcase
-  end
+    always @(*) begin
+        case (current_state)
+            S_0: 
+                if (x == 3'b011)
+                    next_state = S_1;
+                else
+                    next_state = S_0;
+    
+            S_1: 
+                if (x == 3'b111)
+                    next_state = S_2;
+                else
+                    next_state = S_0;
+    
+            S_2: 
+                if (x == 3'b101)
+                    next_state = S_3;
+                else
+                    next_state = S_0;
+    
+            S_3: 
+                next_state = S_0; 
+    
+            default: 
+                next_state = S_0;
+        endcase
+    end
   
   
   //Output Combinational logic 
- always @(*) begin
-   y = (current_state == S3) ? 1'b1 : 1'b0; // y only goes high once current state is S3(11)
- end   
+   always @(*) begin
+    y = (current_state == S_3) ? 1'b1 : 1'b0;  //y only goes high when current state is S3(2'b11)
+   end   
 endmodule
