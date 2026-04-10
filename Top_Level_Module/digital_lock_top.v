128fda9a9e75b578b3576c94d39e6cf7068e09de
antoniov$502 <bryanvasquez797@gmail.com>
2026-04-09 17:47:25 -0700

    Update digital_lock instance in digital_lock_top.v
    Removed pulse_led connection from digital_lock instance.

--- Top_Level_Module/digital_lock_top.v
+++ Top_Level_Module/digital_lock_top.v
@@ -5,7 +5,7 @@ input  [9:0] SW;
 output [9:0] LEDR;
 
 
-digital_lock U1 (.clk(MAX10_CLK1_50), .reset(SW[0]), .x(SW[3:1]), .y(LEDR[0]),  .state(LEDR[3:2]), .pulse_led(LEDR[7]));
+digital_lock U1 (.clk(MAX10_CLK1_50), .reset(SW[0]), .x(SW[3:1]), .y(LEDR[0]),  .state(LEDR[3:2]));
 
 
 endmodule 
