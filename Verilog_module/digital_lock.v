a5e8cf10ba333ae1a7dc347ccd9c9a1f46854724
antoniov$502 <bryanvasquez797@gmail.com>
2026-05-13 11:00:30 -0700

    Fix formatting and comments in digital_lock.v

--- Verilog_module/digital_lock.v
+++ Verilog_module/digital_lock.v
@@ -1,13 +1,13 @@
 module digital_lock(clk, reset, x, y, state);
   
-    input        clk;   //clock signal
-    input        reset; //Aysnchronous active high reset
-    input [2:0]  x;     //3 bit input 
+    input        clk;   // clock signal
+    input        reset; // Aysnchronous active high reset
+    input [2:0]  x;     // 3 bit input 
     output reg   y;
     output [1:0] state;
   
-    //Clock = 50MHz 
-    //50MHz = 50,000,000 cycles per second 
+    // Clock = 50MHz 
+    // 50MHz = 50,000,000 cycles per second 
     // 2 seconds @ 50MHZ = 50,000,000 * 2 = 100,000,000
     
     parameter MAX_COUNT = 100_000_000; 
@@ -16,14 +16,14 @@ module digital_lock(clk, reset, x, y, state);
 
 
     
-    //Counter and pulse generation
+    // Counter and pulse generation
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            counter <= 0;    //set counter to zero
-            pulse   <= 0;   //set pulse to zero
+            counter <= 0;    // set counter to zero
+            pulse   <= 0;   // set pulse to zero
         end else if (counter == MAX_COUNT - 1) begin
-            counter <= 0;    //reset counter when MAX_COUNT is reached
-            pulse   <= 1;   //set pulse to 1    
+            counter <= 0;    // reset counter when MAX_COUNT is reached
+            pulse   <= 1;   // set pulse to 1    
         end else begin
             counter <= counter + 1;  // increment counter each clock cycle
             pulse   <= 0;            // keep pulse low until counter reaches MAX_COUNT again    
@@ -49,14 +49,14 @@ module digital_lock(clk, reset, x, y, state);
     
   
     // State transition logic (clocked)
-    always @(posedge clk or posedge reset) begin  //active high reset
+    always @(posedge clk or posedge reset) begin  // active high reset
         if (reset)
-            current_state <= S_0; //Reset to iniital state
+            current_state <= S_0; // Reset to iniital state
         else if (pulse)
             current_state <= next_state;
     end
   
-  
+  // Input Sequence: 3 → 7 → 5 
   // Combinational logic for next state
     always @(*) begin
         case (current_state)
@@ -64,19 +64,19 @@ module digital_lock(clk, reset, x, y, state);
                 if (x == 3'b011)
                     next_state = S_1;
                 else
-                    next_state = S_0; //Reset to initial state
+                    next_state = S_0; // Reset to initial state
     
             S_1: 
                 if (x == 3'b111)
                     next_state = S_2;
                 else
-                    next_state = S_0; //Reset to initial state
+                    next_state = S_0; // Reset to initial state
     
             S_2: 
                 if (x == 3'b101)
                     next_state = S_3;
                 else
-                    next_state = S_0; //Reset to initial state 
+                    next_state = S_0; // Reset to initial state 
     
             S_3: 
                 next_state = S_0;     // go back to inital state, input doesn't matter 
@@ -87,8 +87,8 @@ module digital_lock(clk, reset, x, y, state);
     end
   
   
-  //Output Combinational logic 
+  // Output Combinational logic 
    always @(*) begin
-    y = (current_state == S_3) ? 1'b1 : 1'b0;  //y only goes high when current state is S3(2'b11)
+    y = (current_state == S_3) ? 1'b1 : 1'b0;  // y only goes high when current state is S3(2'b11)
    end   
 endmodule
