c09067828a5e491b00ec59cd940b230d37098de0
antoniov$502 <bryanvasquez797@gmail.com>
2026-05-15 12:36:09 -0700

    Comment out state wire and HEX0 assignment
    Commented out the state wire and HEX0 assignment for debugging purposes.

--- Top_Level_Module/digital_lock_top.v
+++ Top_Level_Module/digital_lock_top.v
@@ -4,6 +4,14 @@ input        MAX10_CLK1_50; //50MHz clk
 input  [9:0] SW;
 output [9:0] LEDR;
 
+/*  
+wire [1:0] state;
+assign HEX0 = (state == 2'b00) ? 8'hC0:
+				      (state == 2'b01) ? 8'hF9: 
+				      (state == 2'b10) ? 8'hA4: 
+				      (state == 2'b11) ? 8'hB0: 
+				                         8'hC0;
+*/
 
 digital_lock U1 (.clk(MAX10_CLK1_50), .reset(SW[0]), .x(SW[3:1]), .y(LEDR[0]),  .state(LEDR[3:2]));
 
