18c304755625e0c848cc5cb1b4411db74cf015fd
antoniov$502 <bryanvasquez797@gmail.com>
2026-05-15 11:45:19 -0700

    Add comments for HEX0 decoder and display states
    Added comments for 7-segment display state representation.

--- Top_Level_Module/digital_lock_top.v
+++ Top_Level_Module/digital_lock_top.v
@@ -8,4 +8,13 @@ output [9:0] LEDR;
 digital_lock U1 (.clk(MAX10_CLK1_50), .reset(SW[0]), .x(SW[3:1]), .y(LEDR[0]),  .state(LEDR[3:2]));
 
 
+// Will use 7-segment display to display current state 
+//# HEX0 Decoder: Displays state (0-7)
+//# Note: HEX is Active-Low
+//# 7-Seg Display code (active low)
+//#0: 0xC0 
+//#1: 0xF9
+//#2: 0xA4
+//#3: 0xB0
+
 endmodule 
