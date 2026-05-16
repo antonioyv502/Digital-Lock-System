142a168a8ef8473b2f8dd7dc7f4d58725f8b223d
antoniov$502 <bryanvasquez797@gmail.com>
2026-05-16 12:25:03 -0700

    Refactor testbench signals and update state checks

--- Testbench_module/tb_digital_lock.v
+++ Testbench_module/tb_digital_lock.v
@@ -3,11 +3,11 @@
 module tb_digital_lock;
 
     // Testbench signals
-    reg clk;
-    reg reset;
-    reg [2:0] x;
-    wire y;
-    wire [1:0] state;
+    reg        clk;
+    reg        reset;
+    reg [2:0]  x;
+    wire       y;
+    wire [2:0] state;
 
     // Instantiate the digital_lock module
     digital_lock DUT (
@@ -25,23 +25,22 @@ module tb_digital_lock;
         #10 clk = ~clk;  // Toggle clock every 10 time units (50 MHz)
     end
 
-    //Self Checking testbench
     task apply_and_check;
         input [2:0] in;
-        input [1:0] expected_state;
-        input expected_y;
+        input [2:0] expected_state;
+        input       expected_y;
     
     begin 
-        x = in; 
+        x = in;
         #20; //Wait 1 clock cycle
             
-        if(state != expected_state || y != expected_y) begin //Comparing state to exptected_state 
+        if(state != expected_state || y != expected_y) begin 
             $display("Error @ %0t | x=%b | state=%b y=%b | Got: state=%b y=%b", 
                       $time, in, expected_state, expected_y, state, y);
-            errors = errors + 1;
+				errors = errors + 1;
             
-        end else begin
-            $display("Pass @ %0t | x=%b | state=%b y=%b" , $time, in, state, y);
+        end else begin 
+				$display("Pass @ %0t | x=%b | state=%b y=%b" , $time, in, state, y);
         end 
     end
     endtask
@@ -63,17 +62,22 @@ module tb_digital_lock;
         reset = 0;
         
         // Test sequence
-        apply_and_check(3'b011, 2'b01, 1'b0); //Transition to S1
-        apply_and_check(3'b111, 2'b10, 1'b0); //Transition to S2
-        apply_and_check(3'b101, 2'b11, 1'b1); //Transition to S3
+        apply_and_check(3'b011, 3'b001, 1'b0);   //Transition to S1
+        apply_and_check(3'b111, 3'b010, 1'b0);   //Transition to S2
+        apply_and_check(3'b101, 3'b011, 1'b1);   //Transition to S3 (unlock)
+
         
-        apply_and_check(3'b000, 2'b00, 1'b0); //Transition to S0
-        apply_and_check(3'b100, 2'b00, 1'b0); //Stay in S0, testing to see that the system rejects invalid inputs
+        apply_and_check(3'b000, 3'b000, 1'b0); //Transition to S0 
+        apply_and_check(3'b100, 3'b000, 1'b0); //Stay in S0
         
-        apply_and_check(3'b011, 2'b01, 1'b0); //Transition to S1
-        apply_and_check(3'b111, 2'b10, 1'b0); //Transition to S2
-        apply_and_check(3'b100, 2'b00, 1'b0); //wrong input, go back to S0
-       
+        apply_and_check(3'b011, 3'b001, 1'b0); //Transition to S1
+        apply_and_check(3'b111, 3'b010, 1'b0); //Transition to S2
+        apply_and_check(3'b100, 3'b000, 1'b0); //Transition to S0
+		  
+		apply_and_check(3'b011, 3'b001, 1'b0); //Transition to S1
+		apply_and_check(3'b111, 3'b010, 1'b0); //Transition to S2
+		apply_and_check(3'b010, 3'b000, 1'b0); //Transition to S0
+		 
         if(errors == 0)
             $display("\n ALL TESTS PASSED");
         else
