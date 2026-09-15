14002bb051b044657ce65b717209a2f8081d6a5a
antoniov$502 <bryanvasquez797@gmail.com>
2026-09-15 12:49:06 -0700

    Update digital lock to 2-bit state representation
    Refactor digital lock module to use 2-bit states and remove unused inputs.

--- Verilog_module/digital_lock.v
+++ Verilog_module/digital_lock.v
@@ -1,43 +1,35 @@
-module digital_lock(clk, reset, x, enter, mode, y, state);
+module digital_lock(clk, reset, x, y, state);
   
     input        clk;   // clock signal(50MHz)
     input        reset; // Aysnchronous active high reset
     input [2:0]  x;     // 3 bit input 
-	input        enter;
-	input        mode;  // 1: Program password, 0: unlock
     output reg   y;
-    output [2:0] state;
-
+    output [1:0] state;
 	 
-	// Defining States
-    parameter S_0      = 3'b000;  
-    parameter S_1      = 3'b001;
-    parameter S_2      = 3'b010;
-    parameter S_3      = 3'b011;
+	 // Defining States
+    parameter S_0 = 2'b00;  
+    parameter S_1 = 2'b01;
+    parameter S_2 = 2'b10;
+    parameter S_3 = 2'b11;
 
 
-	// 50MHz = 50,000,000 = 1 second
-	// 2 seonds  @ 50MHz = 50,000,000 * 2 = 100,000,000 counts
+	 // 50MHz = 50,000,000 = 1 second
+	 // 2 seonds  @ 50MHz = 50,000,000 * 2 = 100,000,000 counts
     parameter MAX_COUNT = 100_000_000; // 2 second 
     
-	 
-	// State registers
-    reg [2:0] current_state;
-    reg [2:0] next_state;
+	 // State registers
+    reg [1:0] current_state;
+    reg [1:0] next_state;
     
     // allows us to be able to see the current state of flip flops and helpful for debugging
     assign state = current_state; 
-
-	// Password Storage 
-	reg [2:0] password [0:2];
-	reg [1:0] position; 
 	 
     // Counter and 2 second pulse generation logic
-	reg [26:0] counter;               // 27 bit counter can hold up to 100,000,000(2 seconds)
-	reg 	   pulse;
+	 reg [26:0] counter;               // 27 bit counter can hold up to 100,000,000(2 seconds)
+	 reg 			pulse;
 	 
     always @(posedge clk or posedge reset) begin
-    	if (reset) begin
+        if (reset) begin
             counter <= 0;    // Reset counter to 0 
             pulse   <= 0;    // Pulse starts at 0
         end else if (counter == MAX_COUNT - 1) begin
@@ -49,38 +41,8 @@ module digital_lock(clk, reset, x, enter, mode, y, state);
         end
     end
 
-	// Password Programming Logic 
-	always @ (posedge clk or posedge reset) begin 
-		if (reset) begin
-			position <= 0;
-
-			// default password 
-			password[0] <= 3'b011;
-			password[1] <= 3'b111;
-			password[2] <= 3'b101;
-		end else if (mode && enter) begin 
-			password[position] <= x;
-
-			if (position == 2)
-				position <= 0;
-			else 
-				position <= position + 1;
-		end 
-	end 
-	
-    
-	// Logic for incorrect sequence attempts
-	reg [1:0] error_count; //counts incorrect sequences
 	 
-	always @ (posedge clk or posedge reset) begin 
-		if (reset) begin 
-			error_count <= 0;
-		end else if (current_state == S_3) begin 
-			error_count <= 0;	
-		end else if (current_state != S_0 && next_state == S_0) begin 
-			error_count <= error_count + 1;
-	  	end 
-	end
+
 	
     
   
@@ -88,7 +50,7 @@ module digital_lock(clk, reset, x, enter, mode, y, state);
     always @(posedge clk or posedge reset) begin  // active high reset
         if (reset)
             current_state <= S_0;
-        else                             // change to else if (pulse) for hardware implementation
+        else if (pulse)                         // change to else if (pulse) for hardware implementation
             current_state <= next_state;  
     end
   
@@ -96,40 +58,36 @@ module digital_lock(clk, reset, x, enter, mode, y, state);
     // Input Sequence: 3 → 7 → 5
     // Combinational logic for next state
     always @(*) begin
-		if (mode) begin 
-			next_state = S_0;
-		end else begin 
-			case (current_state) 
-				S_0:
-					if (x == password[0])
-						next_state = S_1;
-					else 
-						next_state = S_0;
-
-				S_1:
-					if (x == password[1])
-						next_state = S_2;
-					else 
-						next_state = S_0;
-				S_2:
-					if (x == password[2])
-						next_state = S_3;
-					else 
-						next_state = S_0;
-				S_3:
-					next_state = S_0;
-					
-				default: begin 
-					next_state = S_0;
-				end 
-			endcase
-		end 
+        case (current_state)
+            S_0: 
+                if (x == 3'b011)
+                    next_state = S_1;
+                else
+                    next_state = S_0;
+    
+            S_1: 
+                if (x == 3'b111)
+                    next_state = S_2;
+                else
+                    next_state = S_0;
+    
+            S_2: 
+                if (x == 3'b101)
+                    next_state = S_3;
+                else
+                    next_state = S_0;
+    
+            S_3: 
+					 next_state = S_0;
+						  	
+            default: 
+                next_state = S_0;
+        endcase						
     end
   
   
-    // Output Combinational logic 
+   // Output Combinational logic 
     always @(*) begin
 		  y = (current_state == S_3) ? 1'b1 : 1'b0;  // y only goes high when current state is S_3(2'b11)
     end   
 endmodule
-	
