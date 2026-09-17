46433ac39ffd75be267689fe68014ea0eb568e07
antoniov$502 <bryanvasquez797@gmail.com>
2026-09-17 12:02:51 -0700

    Add HEX1 output and update digital_lock instantiation
    Updated module to include an additional 7-segment display output (HEX1) and modified the instantiation of digital_lock and seven_seg_decoder. Adjusted the HEX1 assignment to display 'U' for unlocked and 'L' for locked states.

--- Top_Level_Module/digital_lock_top.v
+++ Top_Level_Module/digital_lock_top.v
@@ -1,36 +1,26 @@
-module digital_lock_top(MAX10_CLK1_50, SW, LEDR, HEX0);
+module digital_lock_top(MAX10_CLK1_50, SW, LEDR, HEX0, HEX1);
 
-input  		 MAX10_CLK1_50; //50MHz clk
+input		 MAX10_CLK1_50; //50MHz clk
 input  [9:0] SW;
 output [9:0] LEDR; 
 output [7:0] HEX0;          // 7-segment display
+output [7:0] HEX1;          // 7-segment display
+
 
 
 wire [1:0] state;
 
 
 
-assign HEX0 = (state == 2'b00) ? 8'hC0: // 0
-			  (state == 2'b01) ? 8'hF9: // 1
-			  (state == 2'b10) ? 8'hA4: // 2
-		      (state == 2'b11) ? 8'hB0: // 3
-				                 8'hC0; // default case(0)
+											
+// Display U when unlocked (state 3), otherwise display L (locked)											
+assign HEX1 = (state == 2'b11) ? 8'hC1 : 8'hC7; 
 											
 
 
-digital_lock U1 (.clk(MAX10_CLK1_50), .reset(SW[0]), .x(SW[3:1]), .y(LEDR[0]), .state(state));
+digital_lock 		U0 (.clk(MAX10_CLK1_50), .reset(SW[0]), .x(SW[3:1]), .y(LEDR[0]), .state(state));
 
+seven_seg_decoder 	U1 (.state(state), .hex(HEX0));
 
-	 //# HEX0 Decoder: Displays state (0-7)
-    //# Note: HEX is Active-Low
-    //# 7-Seg Display code (active low)
-    //#0: 0xC0 
-    //#1: 0xF9
-    //#2: 0xA4
-	 //#3: 0xB0
-	 //#4: 0x99
-	 //#5: 0x92
-	 //#6: 0x82
-	 //#7: 0xF8
 
 endmodule 
