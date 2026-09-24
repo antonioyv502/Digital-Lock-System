eccd39ff8e4bc5170446054e3bea0fd046cef4ff
antoniov$502 <bryanvasquez797@gmail.com>
2026-09-23 18:24:53 -0700

    Add attempt output to digital lock module

--- Verilog_module/digital_lock.v
+++ Verilog_module/digital_lock.v
@@ -1,32 +1,43 @@
-module digital_lock(clk, reset, x, y, state);
-  
+module digital_lock(clk, reset, x, y, state, attempt);
+	 
     input        clk;   // clock signal(50MHz)
     input        reset; // Aysnchronous active high reset
     input [2:0]  x;     // 3 bit input 
     output reg   y;
     output [1:0] state;
+	output [2:0] attempt;
 	 
-	 // Defining States
+	 
+	// Defining States
     parameter S_0 = 2'b00;  
     parameter S_1 = 2'b01;
     parameter S_2 = 2'b10;
     parameter S_3 = 2'b11;
-
-
-	 // 50MHz = 50,000,000 = 1 second
-	 // 2 seonds  @ 50MHz = 50,000,000 * 2 = 100,000,000 counts
+	 
+	 
+	// 50MHz = 50,000,000 = 1 second
+	// 2 seonds  @ 50MHz = 50,000,000 * 2 = 100,000,000 counts
     parameter MAX_COUNT = 100_000_000; // 2 second 
+	
     
-	 // State registers
+	// State registers
     reg [1:0] current_state;
     reg [1:0] next_state;
+	 
+	 
+	// Register holds number of incorrect sequence attempts
+	reg [2:0] attempts;
+		
     
     // allows us to be able to see the current state of flip flops and helpful for debugging
     assign state = current_state; 
+
+	assign attempt = attempts;
 	 
     // Counter and 2 second pulse generation logic
 	 reg [26:0] counter;               // 27 bit counter can hold up to 100,000,000(2 seconds)
 	 reg 			pulse;
+	 
 	 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
@@ -48,12 +59,26 @@ module digital_lock(clk, reset, x, y, state);
   
     // State transition logic 
     always @(posedge clk or posedge reset) begin  // active high reset
-        if (reset)
+        if (reset) begin 
             current_state <= S_0;
-        else if (pulse)                         // change to else if (pulse) for hardware implementation
-            current_state <= next_state;  
-    end
-  
+        end else if (pulse) begin                       // change to else if (pulse) for hardware implementation
+            current_state <= next_state; 
+		end 		
+    end 
+	 
+	 
+	 
+	 // Sequence Error Detection Logic 
+	 always @(posedge clk or posedge reset) begin 
+	 	 if (reset) begin 
+	 			attempts <= 0;
+	 	 end else if (pulse) begin 
+	 		 if((current_state == S_1 && next_state == S_0) || (current_state == S_2 && next_state == S_0)) begin 
+	 				attempts <= attempts + 1;
+	 		 end 
+	 	 end  
+	 end  
+   
   
     // Input Sequence: 3 → 7 → 5
     // Combinational logic for next state
@@ -78,12 +103,13 @@ module digital_lock(clk, reset, x, y, state);
                     next_state = S_0;
     
             S_3: 
-					 next_state = S_0;
+				next_state = S_0;
 						  	
             default: 
                 next_state = S_0;
         endcase						
     end
+	 
   
   
    // Output Combinational logic 
